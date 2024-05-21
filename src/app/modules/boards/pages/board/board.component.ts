f4eab8eaf25810d46d42fe3c1bd077738dd4dd5c
harol <harol@LAPTOP-IQ42RQOH>
2024-05-21 01:04:44 -0500

    V4 commit

--- src/app/modules/boards/pages/board/board.component.ts
+++ src/app/modules/boards/pages/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   CdkDragDrop,
   moveItemInArray,
@@ -7,7 +7,11 @@ import {
 import { Dialog } from '@angular/cdk/dialog';
 import { TodoDialogComponent } from '@boards/components/todo-dialog/todo-dialog.component';
 
-import { ToDo, Column } from '@models/todo.model';
+import { CardService } from '@services/card.service';
+import { BoardsService } from '@services/boards.service';
+import { ActivatedRoute } from '@angular/router';
+import { Board } from '@models/board.model';
+import { Card } from '@models/card.model';
 
 @Component({
   selector: 'app-board',
@@ -23,48 +27,65 @@ import { ToDo, Column } from '@models/todo.model';
     `,
   ],
 })
-export class BoardComponent {
-  columns: Column[] = [
-    {
-      title: 'ToDo',
-      todos: [
-        {
-          id: '1',
-          title: 'Make dishes',
-        },
-        {
-          id: '2',
-          title: 'Buy a unicorn',
-        },
-      ],
-    },
-    {
-      title: 'Doing',
-      todos: [
-        {
-          id: '3',
-          title: 'Watch Angular Path in Platzi',
-        },
-      ],
-    },
-    {
-      title: 'Done',
-      todos: [
-        {
-          id: '4',
-          title: 'Play video games',
-        },
-      ],
-    },
-  ];
+export class BoardComponent implements OnInit {
 
-  todos: ToDo[] = [];
-  doing: ToDo[] = [];
-  done: ToDo[] = [];
 
-  constructor(private dialog: Dialog) {}
+  board: Board | null = null;
 
-  drop(event: CdkDragDrop<ToDo[]>) {
+  // columns: Column[] = [
+  //   {
+  //     title: 'ToDo',
+  //     todos: [
+  //       {
+  //         id: '1',
+  //         title: 'Make dishes',
+  //       },
+  //       {
+  //         id: '2',
+  //         title: 'Buy a unicorn',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Doing',
+  //     todos: [
+  //       {
+  //         id: '3',
+  //         title: 'Watch Angular Path in Platzi',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Done',
+  //     todos: [
+  //       {
+  //         id: '4',
+  //         title: 'Play video games',
+  //       },
+  //     ],
+  //   },
+  // ];
+
+  // todos: ToDo[] = [];
+  // doing: ToDo[] = [];
+  // done: ToDo[] = [];
+
+  constructor(private dialog: Dialog,
+    private route: ActivatedRoute,
+    private boardsService: BoardsService,
+    private cardsService: CardService
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('boardId');
+      if (id) {
+        this.getBoard(id);
+      }
+    });
+  }
+
+  drop(event: CdkDragDrop<Card[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
@@ -79,25 +100,44 @@ export class BoardComponent {
         event.currentIndex
       );
     }
+    const position = this.boardsService.getPosition(event.container.data,event.currentIndex);
+    const card = event.container.data[event.currentIndex];
+    const listId = event.container.id;
+    this.updateCard(card, position, listId);
   }
 
   addColumn() {
-    this.columns.push({
-      title: 'New Column',
-      todos: [],
-    });
+    // this.columns.push({
+    //   title: 'New Column',
+    //   todos: [],
+    // });
   }
 
-  openDialog(todo: ToDo) {
+  openDialog(card: Card) {
+    console.log(card)
     const dialogRef = this.dialog.open(TodoDialogComponent, {
       minWidth: '300px',
       maxWidth: '50%',
       data: {
-        todo: todo,
+        card: card,
       },
     });
     dialogRef.closed.subscribe((output) => {
       console.log(output);
+    });
+  }
+
+  private getBoard(id: string) {
+    this.boardsService.getBoard(id)
+    .subscribe(board => {
+      this.board = board;
+    });
+  }
+
+  private updateCard(card: Card, position: number, listId:string | number) {
+    this.cardsService.update(card.id, {position, listId})
+    .subscribe((cardUpdate) => {
+      console.log(cardUpdate)
     });
   }
 }
